a1c107c6911e935decf60d933682b55067c995c4
Huihuawk <huihua_wk@163.com>
2018-08-10 15:57:43 +0800

    Data types and variables

--- start/hellopy.py
+++ start/hellopy.py
@@ -1,5 +1,55 @@
 #!/usr/bin/env python3
 
-name = input('input your lover`s name:')
+a = 0x0011
 
-print(name, 'is so beautiful.')
+if a > 0:
+  print(a)
+else:
+  print(-a)
+
+print('I\'m learning\nPython.')
+
+print(r'''line1 'kk'
+line2
+line3''')
+
+True
+
+print(3 > 2)
+
+True and True
+
+True and False
+
+False and False
+
+True or False
+
+False or False
+
+5 > 3 or 1 > 3
+
+not 1 > 3
+
+if None or 0:
+  print(5 > 2)
+else:
+  print('wtf')
+
+a = 'doudou'
+b = a
+b = 'keke'
+print('a:', a)
+print('b:', b)
+
+print(10 / 3)
+print(14 // 3)
+print(9 / 3)
+print(14 % 3)
+
+# a = { name: 'doudou' }
+# print('a.name:', a)
+# b = a
+# b.name = 'keke'
+# print('a.name:', a)
+# print('b.name:', b)
