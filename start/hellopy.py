4ca11bca9acd46b952c4d32a0f7b5a22d9935aaf
Huihuawk <huihua_wk@163.com>
2018-08-13 16:55:44 +0800

    string Unicode

--- start/hellopy.py
+++ start/hellopy.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 a = 0x0011
 
@@ -47,9 +48,52 @@ print(14 // 3)
 print(9 / 3)
 print(14 % 3)
 
-# a = { name: 'doudou' }
-# print('a.name:', a)
-# b = a
-# b.name = 'keke'
-# print('a.name:', a)
-# print('b.name:', b)
+ord('0')
+
+ord('a')
+
+ord('A')
+
+ord('z')
+
+ord('中')
+
+chr(66)
+
+chr(20013)
+
+chr(25991)
+
+'\u4e2d\u6587'
+
+x = b'ABC'
+
+x
+
+c = 'ABC'
+
+'ABC'.encode('ascii')
+
+'ABC'.encode('ascii') == x
+
+'中文'.encode('utf-8')
+
+b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8')
+
+len('中文')
+
+len(b'ABC')
+
+len('中文'.encode('utf-8'))
+
+len(b'\xe4\xb8\xad\xe6\x96\x87')
+
+# 占位符	替换内容
+# %d	整数
+# %f	浮点数
+# %s	字符串
+# %x	十六进制整数
+
+print('%s is the best - %d percent' % ('doudou', 100))
+
+print('Hello, {0}, 成绩提升了 {1:.1f}%'.format('小明', 17.125))
